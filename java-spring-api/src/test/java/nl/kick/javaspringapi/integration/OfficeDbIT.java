924f7d53159603a6f0726a89cc7c79c5d454b9f1
chris2301 <christiaanschop@hotmail.com>
2024-03-17 14:25:55 +0100

    add spring integration test with DB

--- java-spring-api/src/test/java/nl/kick/javaspringapi/integration/OfficeDbIT.java
+++ java-spring-api/src/test/java/nl/kick/javaspringapi/integration/OfficeDbIT.java
@@ -3,22 +3,18 @@ package nl.kick.javaspringapi.integration;
 import nl.kick.javaspringapi.model.Office;
 import nl.kick.javaspringapi.service.OfficeRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-//@WebMvcTest(HotelOfficeController.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
-public class OfficeItWithDatabase {
+public class OfficeDbIT {
 
 
     //TODO implement liquibase and test without testdatabase
@@ -31,7 +27,6 @@ public class OfficeItWithDatabase {
 
     @Test
     public void testGetOffices() throws Exception {
-        // Arrange
         Long officeId = 1L;
         Office expectedOffice = new Office.Builder()
                 .id(officeId)
@@ -41,7 +36,6 @@ public class OfficeItWithDatabase {
 
         officeRepository.save(expectedOffice);
 
-        // Act & Assert
         mockMvc.perform(MockMvcRequestBuilders.get("/rest/hoteloffice/office/{id}", officeId))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(officeId))
